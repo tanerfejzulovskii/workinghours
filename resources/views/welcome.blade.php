32b6156a68a61644a9a66564a7c45a2f3ab059c7
Taner Fejzulovski <taner.fejzulovski@interworks.local>
2019-01-23 16:12:00 +0100

    WH - First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Working hours</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="container mx-auto">
+            Hello World
+        </div>
+    </body>
+</html>
